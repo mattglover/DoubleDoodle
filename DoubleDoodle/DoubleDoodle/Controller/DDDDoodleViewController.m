8e3d0ae6afda0b364b0a4211c8df79414e1bb636
Matt Glover <matt@duchysoftware.com>
2014-02-15 12:19:11 +0000

    Setup DoodleView in DoodleViewController

--- DoubleDoodle/DoubleDoodle/Controller/DDDDoodleViewController.m
+++ DoubleDoodle/DoubleDoodle/Controller/DDDDoodleViewController.m
@@ -9,6 +9,10 @@
 #import "DDDDoodleViewController.h"
 #import "DDDDoodleView.h"
 
+static CGFloat const kDoodleViewInsetX = 10.0f;
+static CGFloat const kDoodleViewInsetY = 40.0f;
+static CGFloat const kDoodleViewOriginPositionY = 10.0f;
+
 @interface DDDDoodleViewController ()
 
 @property (nonatomic, strong) DDDDoodleView *doodleView;
@@ -27,20 +31,35 @@
 // Designated Initializer
 - (id)initWithXML:(NSString *)xml {
   if (self = [super init]) {
-    
+  
   }
   return self;
-}
-
-#pragma mark - View Lifecycle
-- (void)viewDidLoad {
-  [super viewDidLoad];
 }
 
 #pragma mark - Memory Warning
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
   NSLog(@"%s", __FUNCTION__);
+}
+
+#pragma mark - View Lifecycle
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  
+  UIColor *backgroundColor = [UIColor blackColor];
+  UIColor *lineStrokeColor = [UIColor whiteColor];
+  [self setupDoodleViewWithBackgroundColor:backgroundColor lineStrokeColor:lineStrokeColor];
+}
+
+#pragma mark - Setup
+- (void)setupDoodleViewWithBackgroundColor:(UIColor *)backgroundColor lineStrokeColor:(UIColor *)lineStrokeColor {
+ 
+  CGRect entireViewBounds = self.view.bounds;
+  CGRect doodleViewFrame = CGRectInset(entireViewBounds, kDoodleViewInsetX, kDoodleViewInsetY);
+  doodleViewFrame.origin.y = kDoodleViewOriginPositionY;
+  self.doodleView = [[DDDDoodleView alloc] initWithFrame:doodleViewFrame];
+  
+  [self.view addSubview:self.doodleView];
 }
 
 
